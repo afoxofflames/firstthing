1762190a80dcae78ee05a91d6affd8053acf6417
YOUR NAME <YOUR@EMAIL.com>
2021-05-24 09:54:37 -0600

    added_nav

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   #get /about
   get "about", to: "about#index"
 
+  #main page can also be called "root", so it would be root to:"main#index"
   get "/", to: "main#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
