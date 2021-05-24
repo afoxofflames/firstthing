2a266723a964b9b267e7a16104e4b94485d0cf54
YOUR NAME <YOUR@EMAIL.com>
2021-05-24 10:21:39 -0600

    added_shortcuts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  #get /about
+  #can also do "about-us", to: "about#index", as: :about 
+  #and it will get the about page route
   get "about", to: "about#index"
 
   #main page can also be called "root", so it would be root to:"main#index"
-  get "/", to: "main#index"
+  root to: "main#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
