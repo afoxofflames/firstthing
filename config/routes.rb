a2df780dff0b264b1982a638520bceccf28a92ed
YOUR NAME <YOUR@EMAIL.com>
2021-05-25 14:36:54 -0600

    added_user

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   #and it will get the about page route
   get "about", to: "about#index"
 
+  get "sign_up", to: "registrations#new"
+  post"sign_up", to: "registrations#create"
+
   #main page can also be called "root", so it would be root to:"main#index"
   root to: "main#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
